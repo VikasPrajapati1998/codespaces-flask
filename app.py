7d682f2ab61cb065f2b0894feba992b27a1c4051
Er. Vikas Prajapati <81795265+VikasPrajapati1998@users.noreply.github.com>
2023-09-03 12:13:58 +0000

    home url

--- app.py
+++ app.py
@@ -3,5 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
+    """
+    This is the home url functoin.
+
+    args: None
+    returns: html
+    """
     return render_template("index.html", title="Hello")
+
+if __name__ == "__main__":
+    app.run()
